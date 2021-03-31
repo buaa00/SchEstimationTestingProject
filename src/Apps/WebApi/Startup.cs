90e9f6439171d7aac7009fd14c0b8cc94856e496
Vukoman <vukomans96@yahoo.com>
2021-03-31 14:43:17 +0200

    Dockerized WebApi application

--- src/Apps/WebApi/Startup.cs
+++ src/Apps/WebApi/Startup.cs
@@ -40,9 +40,10 @@ namespace SchEstimationTestingProject.Apps.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchEstimationTestingProject.Apps.WebApi v1"));
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchEstimationTestingProject.Apps.WebApi v1"));
 
             app.UseHttpsRedirection();
 
